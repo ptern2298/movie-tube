f4a67609677a08a9d263ee23b2a657d186c464d0
Pavlo <p.tern2298@gmail.com>
2024-01-12 19:20:26 -0500

    - Added Banner Component; - Fixed styles; - Added Page Header Component;

--- src/containers/HomePage.tsx
+++ src/containers/HomePage.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import MoviesRowComponent from "../components/MoviesRowComponent";
 import requests from "../utils/Requests";
+import PageHeader from "../components/PageHeader";
+import Banner from "../components/Banner";
+import '../App.css';
 function HomePage() {
     return(
         <div>
+            <PageHeader/>
+            <Banner/>
             <MoviesRowComponent title="Trending" fetchURL={requests.fetchTrending}/>
             <MoviesRowComponent title="Top Rated" fetchURL={requests.fetchTopRated}/>
             <MoviesRowComponent title="Originals" fetchURL={requests.fetchNetflixOriginals}/>
             <MoviesRowComponent title="Action Movies" fetchURL={requests.fetchActionMovies}/>
             <MoviesRowComponent title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
             <MoviesRowComponent title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
-            <MoviesRowComponent title="Horror Movies" fetchURL={requests.fetchHorrorMovies} isLarge={false}/>
-            <MoviesRowComponent title="Documentaries" fetchURL={requests.fetchDocumentaries} isLarge={false}/>
+            <MoviesRowComponent title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
+            <MoviesRowComponent title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
         </div>
     );
 }
